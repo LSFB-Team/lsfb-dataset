e2534ad9f9da088b9b590cf17cfc41823051e0e2
Jérôme Fink <jerome.fink@unamur.be>
2022-01-26 16:59:14 +0100

    [upload] testing test upload on pypi test

--- lsfb-dataset/setup.py
+++ lsfb-dataset/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="lsfb-dataset",
-    version="0.1.0",
+    version="0.0.2",
     author="Jérôme Fink",
     author_email="jerome.fink@unamur.be",
     description="A companion library for the LSFB-dataset",
