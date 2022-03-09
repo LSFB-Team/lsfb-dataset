9c5c2d2077066edd89cd151b3c11eff97371ef1c
Pierre Poitier <hanofound@hotmail.com>
2022-03-09 07:09:21 +0100

    Adding mogrifier LSTM and Phased LSTM

--- lsfb-dataset/src/lsfb_dataset/models/rnn/rnn_classifier.py
+++ lsfb-dataset/src/lsfb_dataset/models/rnn/rnn_classifier.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 from .lstm import LSTM
 from .edrn import EDRN
+from .plstm import PhasedLSTM
+from .mogrifier_lstm import MogrifierLSTM
 
 
 class LSTMClassifier(nn.Module):
@@ -60,6 +62,58 @@ class EDRNClassifier(nn.Module):
             hidden_size=hidden_size,
             substates=substates,
         )
+
+        self.fc = nn.Linear(hidden_size, n_classes)
+
+    def forward(self, x):
+        outputs, _ = self.rnn(x)
+        return self.fc(outputs)
+
+
+class PhasedLSTMClassifier(nn.Module):
+    def __init__(
+            self,
+            input_size,
+            hidden_size,
+            n_classes,
+            alpha=1e-3,
+            taux_max=3.0,
+            r_on=5e-2
+    ):
+        super(PhasedLSTMClassifier, self).__init__()
+
+        self.input_size = input_size
+        self.n_classes = n_classes
+
+        self.rnn = PhasedLSTM(
+            input_size,
+            hidden_size,
+            alpha=alpha,
+            taux_max=taux_max,
+            r_on=r_on,
+        )
+
+        self.fc = nn.Linear(hidden_size, n_classes)
+
+    def forward(self, x):
+        outputs, _ = self.rnn(x)
+        return self.fc(outputs)
+
+
+class MogrifierLSTMClassifier(nn.Module):
+    def __init__(
+            self,
+            input_size,
+            hidden_size,
+            n_classes,
+            mogrify_steps=5,
+    ):
+        super(MogrifierLSTMClassifier, self).__init__()
+
+        self.input_size = input_size
+        self.n_classes = n_classes
+
+        self.rnn = MogrifierLSTM(input_size, hidden_size, mogrify_steps=mogrify_steps)
 
         self.fc = nn.Linear(hidden_size, n_classes)
 
