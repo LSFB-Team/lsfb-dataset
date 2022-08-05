b6161aa4d0dc9802dced0184a10e46ef4112cfed
Pierre Poitier <hanofound@hotmail.com>
2022-08-05 09:48:41 +0200

    Rework gui

--- lsfb-dataset/src/lsfb_dataset/gui/handlers/video.py
+++ lsfb-dataset/src/lsfb_dataset/gui/handlers/video.py
@@ -1,9 +1,10 @@
+import numpy as np
 from cv2 import cv2
 from PIL import Image, ImageTk, ImageOps
 import tkinter as tk
-import pandas as pd
 from lsfb_dataset.visualisation.features import get_annotations_img
-from lsfb_dataset.visualisation.features import draw_pose_landmarks, draw_hands_landmarks
+from lsfb_dataset.visualisation.features import draw_pose_landmarks, draw_hands_landmarks,\
+    draw_face_mesh, draw_face_landmarks
 
 
 class VideoCapture:
@@ -65,9 +66,16 @@ class VideoHandler:
         self.right_annots = None
         self.left_annots = None
 
+        self.right_hand = True
+        self.left_hand = True
+
         self.display_video = True
         self.display_skeleton = False
         self.display_hands = False
+        self.display_face_mesh = False
+        self.display_face_contours = False
+
+        self.post_processing = True
 
         self.update()
 
@@ -90,12 +98,38 @@ class VideoHandler:
     def toggle_display_hands(self, val):
         self.display_hands = val
 
+    def toggle_display_face_mesh(self, val):
+        self.display_face_mesh = val
+
+    def toggle_display_face_contours(self, val):
+        self.display_face_contours = val
+
+    def toggle_post_processing(self, val):
+        self.post_processing = val
+        self.gui.parent.handler.set_post_processing(val)
+
+    def update_hands(self, right_hand: bool, left_hand: bool):
+        self.right_hand = right_hand
+        self.left_hand = left_hand
+
+        self._clear_annot_img(not right_hand, not left_hand)
+
+    def move(self, offset):
+        if self.cap is not None:
+            self.cap.goto_frame(self.cap.current_frame + offset)
+
     def update(self):
         if self.cap is not None and not self.pause:
             success, frame = self.cap.get_frame()
 
-            skeletons = self.gui.parent.handler.skeleton
-            hands = self.gui.parent.handler.hands_landmarks
+            if not self.display_video:
+                frame = np.zeros(shape=frame.shape, dtype='uint8')
+
+            landmarks = self.gui.parent.handler.landmarks
+
+            skeletons = landmarks.get('pose')
+            hands = landmarks.get('hands')
+            face = landmarks.get('face')
 
             if self.display_skeleton and skeletons is not None:
                 draw_pose_landmarks(frame, skeletons.iloc[self.cap.current_frame].values.reshape((-1, 2)))
@@ -103,11 +137,19 @@ class VideoHandler:
             if self.display_hands and hands is not None:
                 draw_hands_landmarks(frame, hands.iloc[self.cap.current_frame].values.reshape((-1, 2)))
 
+            if self.display_face_mesh and face is not None:
+                draw_face_mesh(frame, face.iloc[self.cap.current_frame].values.reshape((-1, 2)))
+
+            if self.display_face_contours and face is not None:
+                draw_face_landmarks(frame, face.iloc[self.cap.current_frame].values.reshape((-1, 2)), (255, 0, 0))
+
             if success:
                 if frame is not None:
                     self.update_video_img(Image.fromarray(frame))
-                    self.update_right_annotation_img()
-                    self.update_left_annotation_img()
+                    if self.right_hand:
+                        self.update_right_annotation_img()
+                    if self.left_hand:
+                        self.update_left_annotation_img()
 
             else:
                 self.cap = None
@@ -122,12 +164,23 @@ class VideoHandler:
 
     def update_right_annotation_img(self):
         if self.right_annots is not None:
-            img = Image.fromarray(get_annotations_img(self.right_annots, self.cap.current_time, width=250, height=300))
+            img = Image.fromarray(get_annotations_img(self.right_annots, self.cap.current_time, width=250, height=250))
             self.right_annot_photo = ImageTk.PhotoImage(image=img)
             self.gui.right_true.create_image(0, 0, image=self.right_annot_photo, anchor=tk.NW)
 
     def update_left_annotation_img(self):
         if self.left_annots is not None:
-            img = Image.fromarray(get_annotations_img(self.left_annots, self.cap.current_time, width=250, height=300))
+            img = Image.fromarray(get_annotations_img(self.left_annots, self.cap.current_time, width=250, height=250))
             self.left_annot_photo = ImageTk.PhotoImage(image=img)
             self.gui.left_true.create_image(0, 0, image=self.left_annot_photo, anchor=tk.NW)
+
+    def _clear_annot_img(self, right: bool, left: bool):
+        if not right and not left:
+            return
+
+        empty_img = Image.fromarray(np.full(shape=(250, 250, 3), fill_value=0, dtype='uint8'))
+        self.empty_photo = ImageTk.PhotoImage(image=empty_img)
+        if right:
+            self.gui.right_true.create_image(0, 0, image=self.empty_photo, anchor=tk.NW)
+        if left:
+            self.gui.left_true.create_image(0, 0, image=self.empty_photo, anchor=tk.NW)
