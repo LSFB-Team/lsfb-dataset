567ee7548286ae11ed6dfe65fda95d13b5ef2670
jefidev <jerome.fink@unamur.be>
2022-10-17 17:36:32 +0200

    Architecture proposal for lsfb datasets

--- lsfb-dataset/src/lsfb_dataset/datasets/lsfb_isol/base.py
+++ lsfb-dataset/src/lsfb_dataset/datasets/lsfb_isol/base.py
@@ -3,10 +3,11 @@ import pandas as pd
 from tqdm import tqdm
 from datetime import datetime
 
-from .types import *
+from lsfb_dataset.datasets.types import *
 from typing import Optional, List
-from ..utils.landmarks import load_pose_landmarks, load_hands_landmarks, pad_landmarks
-from ..utils.datasets import split_isol, mini_sample, create_mask
+from lsfb_dataset.utils.landmarks import load_pose_landmarks, load_hands_landmarks, pad_landmarks
+from lsfb_dataset.utils.datasets import split_isol, mini_sample, create_mask
+import abc
 
 
 def _select_videos(videos, lemmes, split: str):
@@ -24,43 +25,10 @@ def _select_videos(videos, lemmes, split: str):
     return videos
 
 
-def _load_landmarks(
-        root: str,
-        videos: pd.DataFrame,
-        landmarks: List[str],
-        sequence_max_length: int,
-        show_progress: bool,
-):
-    landmark_list = ', '.join(landmarks)
-    print(f'Loading features ({landmark_list}) and labels for each isolated sign...')
-
-    features = []
-    targets = []
-
-    progress_bar = tqdm(videos.iterrows(), total=videos.shape[0], disable=(not show_progress))
-    for index, video in progress_bar:
-        data = []
-
-        for lm_type in landmarks:
-            if lm_type == 'pose':
-                data.append(load_pose_landmarks(root, video['pose']))
-            elif lm_type == 'hand_left':
-                data.append(load_hands_landmarks(root, video['hands'], 'left'))
-            elif lm_type == 'hand_right':
-                data.append(load_hands_landmarks(root, video['hands'], 'right'))
-            else:
-                raise ValueError(f'Unknown landmarks: {lm_type}.')
-
-        features.append(pd.concat(data, axis=1).values[:sequence_max_length])
-        targets.append(video['class'])
-
-    return features, targets
-
-
-class LSFBIsolLandmarks:
+class LSFBIsolBase:
 
     """
-    LSFB_ISOL landmarks Dataset.
+    LSFB_ISOL Base Dataset.
 
     For each clips of LSFB_ISOL, this dataset provides a per-frame 2D-landmarks (skeleton)
     associated with a label for the performed sign (gloss).
@@ -115,12 +83,10 @@ class LSFBIsolLandmarks:
             mask_value: int = 0,
             show_progress=True,
     ):
-        print('-'*10, 'LSFB ISOL DATASET')
-        start_time = datetime.now()
-
         if landmarks is None:
             landmarks = ['pose', 'hand_left', 'hand_right']
 
+        self.root = root
         self.landmarks = landmarks
         self.transform = transform
         self.target_transform = target_transform
@@ -130,50 +96,23 @@ class LSFBIsolLandmarks:
         self.padding = padding
         self.return_mask = return_mask
         self.mask_value = mask_value
+        self.show_progress = show_progress
 
         lemme_list_path = os.path.join(root, lemme_list_path)
         videos_list_path = os.path.join(root, videos_list_path)
 
         lemmes = pd.read_csv(lemme_list_path)
-        lemmes = lemmes.iloc[:lemmes_nb]
+        self.lemmes = lemmes.iloc[:lemmes_nb]
 
         self.videos = pd.read_csv(videos_list_path)
         self.videos = _select_videos(self.videos, lemmes, split)
 
-        self.features, self.targets = _load_landmarks(
-            root,
-            self.videos,
-            landmarks,
-            sequence_max_length,
-            show_progress,
-        )
-        self.labels = lemmes['lemme']
 
-        print('-'*10)
-        print('loading time:', datetime.now() - start_time)
-
+    @abc.abstractmethod
     def __len__(self):
-        return len(self.features)
-
+        pass
+        
+    @abc.abstractmethod
     def __getitem__(self, index):
-        features = self.features[index]
-        target = self.targets[index]
-        pad_value = 0
-
-        if self.padding:
-            pad_value = self.sequence_max_length - len(features)
-            features = pad_landmarks(features, pad_value)
-
-        if self.transform is not None:
-            features = self.transform(features)
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        if self.return_mask:
-            mask = create_mask(self.sequence_max_length, pad_value, self.mask_value)
-            if self.mask_transform is not None:
-                mask = self.mask_transform(mask)
-
-            return features, target, mask
-
-        return features, target
+        pass
+        
