5717b6c8bfa430dc01cd87ee58f926a9880187d1
Pierre Poitier <hanofound@hotmail.com>
2022-10-19 16:27:06 +0200

    Refactor LSFB CONT dataset

--- lsfb-dataset/src/lsfb_dataset/utils/landmarks.py
+++ lsfb-dataset/src/lsfb_dataset/utils/landmarks.py
@@ -4,13 +4,13 @@ from os import path
 from typing import Optional
 
 
-def load_pose_landmarks(root: Optional[str], csv_path: str):
+def load_pose_landmarks(root: Optional[str], csv_path: str) -> pd.DataFrame:
     filepath = path.join(root, csv_path) if root is not None else csv_path
     df = pd.read_csv(filepath, dtype='float32')
     return df
 
 
-def load_hands_landmarks(root: Optional[str], csv_path: str, hands: str):
+def load_hands_landmarks(root: Optional[str], csv_path: str, hands: str) -> pd.DataFrame:
     filepath = path.join(root, csv_path) if root is not None else csv_path
     df = pd.read_csv(filepath, dtype='float32')
     if hands == 'right':
@@ -18,6 +18,20 @@ def load_hands_landmarks(root: Optional[str], csv_path: str, hands: str):
     elif hands == 'left':
         df = df.loc[:, df.columns.str.startswith('LEFT')]
     return df
+
+
+def load_landmarks(video: pd.Series, root: str, landmarks: list[str]) -> np.ndarray:
+    video_landmarks = []
+    for landmark in landmarks:
+        if landmark == 'pose':
+            video_landmarks.append(load_pose_landmarks(root, video['pose']))
+        elif landmark == 'hand_left':
+            video_landmarks.append(load_hands_landmarks(root, video['hands'], 'left'))
+        elif landmark == 'hand_right':
+            video_landmarks.append(load_hands_landmarks(root, video['hands'], 'right'))
+        else:
+            raise ValueError(f'Unknown landmarks: {landmark}.')
+    return pd.concat(video_landmarks, axis=1).values
 
 
 def pad_landmarks(landmarks, padding: int):
