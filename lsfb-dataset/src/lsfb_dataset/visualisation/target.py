402f4a4133b38c94b40936e4448425b6cb7b02da
Pierre Poitier <hanofound@hotmail.com>
2022-10-21 17:13:31 +0200

    Add LSFB CONT lemme segmentation visualization

--- lsfb-dataset/src/lsfb_dataset/visualisation/target.py
+++ lsfb-dataset/src/lsfb_dataset/visualisation/target.py
@@ -1,5 +1,10 @@
+import matplotlib as mpl
+import matplotlib.cm as cm
 import matplotlib.pyplot as plt
+import numpy as np
+
 from ..utils.helpers import get_gaps
+from ..utils.target import get_segments
 
 
 def plot_binary_segmentation(vector, x_lim: tuple[int, int] = None):
@@ -27,3 +32,24 @@ def plot_binary_segmentation_with_coarticulation(vector, x_lim: tuple[int, int] 
 
     for start, end in get_gaps(vector, 1, 2):
         plt.axvspan(start, end, color='red')
+
+
+def plot_lemme_segmentation(
+        segmentation: np.ndarray,
+        x_lim: tuple[int, int] = None,
+        lemmes_nb: int = 100,
+        cmap: str = 'twilight_shifted',
+):
+    if x_lim is None:
+        x_lim = 0, len(segmentation)
+
+    norm = mpl.colors.Normalize(vmin=0, vmax=2+lemmes_nb)
+    cmap = cm.get_cmap(cmap)
+
+    fig, ax = plt.subplots(1, 1, figsize=(30, 4))
+    ax.set_xlim(*x_lim)
+
+    for value, start, end in get_segments(segmentation):
+        if value == 0:
+            continue
+        plt.axvspan(start, end, color=cmap(norm(value)))
