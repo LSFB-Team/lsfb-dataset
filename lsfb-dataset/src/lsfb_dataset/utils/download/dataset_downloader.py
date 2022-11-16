e9f727b020c3467165438e498bab9e5cab7bce96
Pierre Poitier <hanofound@hotmail.com>
2022-11-16 15:07:38 +0100

    Fix LSFB CONT download and refactor downloader

--- lsfb-dataset/src/lsfb_dataset/utils/download/dataset_downloader.py
+++ lsfb-dataset/src/lsfb_dataset/utils/download/dataset_downloader.py
@@ -1,10 +1,13 @@
-import urllib
-import urllib.request
+from urllib.parse import quote, urljoin
+from pathlib import PurePath
+import requests
 import hashlib
 import pandas as pd
 from tqdm import tqdm
 import os
+from os import path
 from typing import List
+from ..datasets import split_cont, split_isol, mini_sample
 
 
 class DatasetDownloader:
@@ -22,8 +25,13 @@ class DatasetDownloader:
             'pose' for pose skeleton (23 landmarks);
             'hands' for hand skeleton (21 landmarks per hand);
             'face' for face skeleton (468 landmarks).
+        split : Select which subset of the dataset should be downloaded. Default = 'all'.
+            'all' for the entire dataset;
+            'train' for the training data only;
+            'test' for the test data only;
+            'mini_sample' for a tiny sample of the dataset.
         include_video : Download the raw video. Default = False.
-        include_raw : Download the raw landmarks (festures) without smoothing and interpolation.
+        include_raw : Download the raw landmarks (features) without smoothing nor interpolation.
             Default = False.
         compute_hash : Verify the hash of the video. Include_video should be set to true to use
             this options. Default = False.
@@ -35,6 +43,7 @@ class DatasetDownloader:
             destination: str,
             dataset: str = "isol",
             landmarks: List[str] = None,
+            split: str = 'all',
             include_video: bool = False,
             include_raw: bool = False,
             compute_hash: bool = False,
@@ -42,6 +51,7 @@ class DatasetDownloader:
     ):
         self.destination = destination
         self.dataset = dataset
+        self.split = split
 
         if landmarks is None:
             self.landmarks = ["pose", "hands"]
@@ -49,10 +59,9 @@ class DatasetDownloader:
             self.landmarks = landmarks
 
         if include_raw:
-            landmarks = self.landmarks
-
-            for landmark in self.landmarks:
-                landmarks.append(landmark + "_raw")
+            landmarks = [*self.landmarks]
+            for landmark in landmarks:
+                self.landmarks.append(landmark + "_raw")
 
         self.include_video = include_video
         self.compute_hash = compute_hash
@@ -70,9 +79,11 @@ class DatasetDownloader:
         """
         csv_path = self._download_csv()
         data = pd.read_csv(csv_path)
+        data = self._select_split(data)
 
-        # processing the CSV file
-        for idx, row in tqdm(data.iterrows(), total=data.shape[0]):
+        progress_bar = tqdm(data.iterrows(), total=data.shape[0])
+        for idx, row in progress_bar:
+            progress_bar.set_postfix_str(row['filename'])
 
             if self.include_video:
                 self.download_video(row)
@@ -81,6 +92,7 @@ class DatasetDownloader:
                 self._download_annotations(row, "annots_left")
                 self._download_annotations(row, "annots_right")
                 self._download_annotations(row, "annots_trad")
+                self._download_segmentation_vectors()
 
             for landmark in self.landmarks:
                 if type(row[landmark]) == str and row[landmark] != "":
@@ -92,29 +104,26 @@ class DatasetDownloader:
         if self.dataset == "isol":
             csv_files.append("lemmes.csv")
             csv_files.append("clips.csv")
-
-            detailed_csv_destination = os.path.join(self.destination, "clips.csv")
+            metadata_dest = path.join(self.destination, "clips.csv")
 
         elif self.dataset == "cont":
-            csv_files.append("valid_videos.csv")
-            detailed_csv_destination = os.path.join(self.destination, "valid_videos.csv")
+            csv_files.append("videos.csv")
+            metadata_dest = path.join(self.destination, "videos.csv")
 
         else:
             raise ValueError(f'Unknown dataset: {self.dataset}')
 
-        for elem in csv_files:
-            url = os.path.join(self.src, elem)
-            destination = os.path.join(self.destination, elem)
+        for origin_path in csv_files:
+            destination = os.path.join(self.destination, origin_path)
+            self._download_file(origin_path, destination)
 
-            urllib.request.urlretrieve(url, destination)
-
-        return detailed_csv_destination
+        return metadata_dest
 
     def download_video(self, row):
         """
         Download the video from the source url if the video was not yet downloaded by the client.
         This function also checks the integrity of the video already downloaded. If the md5 sum doesn't match
-        the video is redownloaded.
+        the video is re-downloaded.
 
         Input:
 
@@ -122,21 +131,17 @@ class DatasetDownloader:
         """
 
         if self.dataset == 'isol':
-            location = row["video"]
+            video_path = row["video"]
         else:
-            location = row["filepath"]
+            video_path = row["filepath"]
 
-        # Test if the video exists
-        video_url = os.path.join(self.src, urllib.parse.quote(location))
-        video_destination = os.path.join(self.destination, location)
+        video_destination = os.path.join(self.destination, video_path)
 
-        self._create_directories(video_destination)
-
-        if not os.path.exists(video_destination):
-            urllib.request.urlretrieve(video_url, video_destination)
+        if not os.path.isfile(video_destination):
+            self._download_file(video_path, video_destination)
 
         elif os.path.getsize(video_destination) == 0:
-            urllib.request.urlretrieve(video_url, video_destination)
+            self._download_file(video_path, video_destination)
 
         elif self.dataset == "cont" and self.compute_hash:
             # Check md5sum
@@ -144,46 +149,65 @@ class DatasetDownloader:
             src_md5 = row["md5"]
 
             if dest_md5 != src_md5:
-                urllib.request.urlretrieve(video_url, video_destination)
+                self._download_file(video_path, video_destination)
 
     def _download_annotations(self, row, col_name):
         """
-        Download the annotations file for a video. There is one file for each hand.
+        Download the annotations file for a video.
+        There is one file for each hand.
 
         Input:
         row : The dataframe row containing the information for that video.
         hand : The hand you want to download the annotations for. Either "right_hand" or "left_hand".
         """
 
-        location = row[col_name]
-
-        annotation_url = os.path.join(self.src, urllib.parse.quote(location))
-        annotation_destination = os.path.join(self.destination, location)
-
-        self._create_directories(annotation_destination)
-
+        annot_path = row[col_name]
+        annotation_destination = os.path.join(self.destination, annot_path)
         if not os.path.exists(annotation_destination):
-            urllib.request.urlretrieve(annotation_url, annotation_destination)
+            self._download_file(annot_path, annotation_destination)
 
-    def _download_landmarks(self, relative_path):
-        landmarks_url = os.path.join(self.src, urllib.parse.quote(relative_path))
-        landmarks_destination = os.path.join(self.destination, relative_path)
+    def _download_segmentation_vectors(self):
+        destinations = [
+            (f'annotations/vectors/{x}.pck', path.join(self.destination, 'annotations/vectors', f'{x}.pck'))
+            for x in ['activity', 'binary', 'binary_with_coarticulation']
+        ]
+        for origin, dest in destinations:
+            self._download_file(origin, dest)
 
-        self._create_directories(landmarks_destination)
-
+    def _download_landmarks(self, landmark_path):
+        landmarks_destination = os.path.join(self.destination, landmark_path)
         if not os.path.exists(landmarks_destination):
-            urllib.request.urlretrieve(landmarks_url, landmarks_destination)
+            self._download_file(landmark_path, landmarks_destination)
 
-    def _create_directories(self, path):
-        """
-        Create the directories in the destination if they don't exist.
+    def _download_file(self, origin: str, destination: str):
+        url = urljoin(f'{self.src}/', quote(origin))
 
-        Input:
-        path : The path to the file you want to create the directories for.
-        """
+        req = requests.get(url, allow_redirects=False, timeout=60)
+        if req.status_code != 200:
+            raise FileNotFoundError(f"""
+            Could not download the desired file: {origin}.
+            Wrong  HTTP status ({req.status_code}) for request ({url}).
+            """)
 
-        directories = os.sep.join(path.split(os.sep)[:-1])
+        os.makedirs(str(PurePath(destination).parent), exist_ok=True)
+        with open(destination, 'wb') as file:
+            file.write(req.content)
 
-        # Creating missing directories in destination
-        if not os.path.exists(directories):
-            os.makedirs(directories)
+    def _select_split(self, data):
+        if self.split == 'all':
+            return data
+
+        if self.split == 'mini_sample':
+            return mini_sample(data)
+
+        if self.dataset == 'isol':
+            train, test = split_isol(data)
+        else:
+            train, test = split_cont(data)
+
+        if self.split == 'train':
+            return train
+        elif self.split == 'test':
+            return test
+        else:
+            raise ValueError(f'Unknown split: {self.split}')
