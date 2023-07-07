81704146c13aa7b478c14a8aab03467fc2fab51d
Jérôme Fink <jerome.fink@unamur.be>
2023-07-07 08:55:31 +0200

    Adding the new fixture in the test config and updating test suite

--- lsfb-dataset/tests/datasets/lsfb_isol/test_config.py
+++ lsfb-dataset/tests/datasets/lsfb_isol/test_config.py
@@ -3,7 +3,7 @@ from lsfb_dataset.datasets.lsfb_isol import LSFBIsolConfig, LSFBIsolLandmarks
 
 
 def test_lsfb_isol_loader(mock_lsfb_isol_path_v2):
-    config = LSFBIsolConfig(mock_lsfb_isol_path)
+    config = LSFBIsolConfig(mock_lsfb_isol_path_v2)
     isolLoader = LSFBIsolLandmarks(config=config)
 
     assert len(isolLoader) == 300
